6eccddec531717261bfba6da6ae1ab138147f19e
jillurcf <jrtushar@gmail.comm>
2025-12-19 08:50:47 -0800

    navBar updated

--- app/home/Banner.tsx
+++ app/home/Banner.tsx
@@ -1,31 +1,34 @@
-import React from 'react';
-import { Carousel } from 'antd';
+"use client"
 
-const imgStyle: React.CSSProperties = {
-  width: '100%',
-  height: "60vh", 
-  objectFit: 'cover', // Ensures the image covers the container without distortion
-};
+import React from "react"
+import { Carousel } from "antd"
+import Image from "next/image"
+
+const images = [
+  "/BannerImg5.jpg",
+  "/BannerImg1.jpg",
+  "/BannerImg2.jpg",
+  "/BannerImg3.jpg",
+  "/BannerImg4.jpg",
+  "/BannerImg7.jpg",
+]
 
 const Banner: React.FC = () => {
-  const images = [
-    '/BannerImg5.jpg',
-    '/BannerImg1.jpg',
-    '/BannerImg2.jpg',
-    '/BannerImg3.jpg',
-    '/BannerImg4.jpg',
-    '/BannerImg7.jpg',
-  ];
-
   return (
     <Carousel arrows autoplay autoplaySpeed={5000}>
       {images.map((src, index) => (
-        <div key={index}>
-          <img src={src} alt={`Banner ${index + 1}`} style={imgStyle} />
+        <div key={index} className="w-full h-[60vh] relative">
+          <Image
+            src={src}
+            alt={`Banner ${index + 1}`}
+            fill
+            style={{ objectFit: "cover" }}
+            priority
+          />
         </div>
       ))}
     </Carousel>
-  );
-};
+  )
+}
 
-export default Banner;
+export default Banner
