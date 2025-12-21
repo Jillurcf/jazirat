a605174f7aae91f2189abaf966ddfdf56e76ce52
jillurcf <jrtushar@gmail.comm>
2025-12-21 05:18:32 -0800

    Banner img modified

--- app/home/Banner.tsx
+++ app/home/Banner.tsx
@@ -1,11 +1,49 @@
+// "use client"
+
+// import React from "react"
+// import { Carousel } from "antd"
+// import Image from "next/image"
+
+// const images = [
+//     "/BannerImg5.jpg",
+//   "/BannerOriginal.jpg",
+//   "/BannerImg1.jpg",
+//   "/BannerImg2.jpg",
+//   "/BannerImg3.jpg",
+//   "/BannerImg4.jpg",
+//   "/BannerImg7.jpg",
+// ]
+
+// const Banner: React.FC = () => {
+//   return (
+//     <Carousel arrows autoplay autoplaySpeed={5000}>
+//       {images.map((src, index) => (
+//         <div key={index} className="w-full h-[60vh] relative">
+//           <Image
+//             src={src}
+//             alt={`Banner ${index + 1}`}
+//             fill
+//             style={{ objectFit: "cover" }}
+//             priority
+//           />
+//         </div>
+//       ))}
+//     </Carousel>
+//   )
+// }
+
+// export default Banner
+
+
 "use client"
 
 import React from "react"
 import { Carousel } from "antd"
 import Image from "next/image"
 
-const images = [
-    "/BannerImg5.jpg",
+const Banner: React.FC = () => {
+  const desktopImages = [
+  "/BannerImg5.jpg",
   "/BannerOriginal.jpg",
   "/BannerImg1.jpg",
   "/BannerImg2.jpg",
@@ -14,21 +52,52 @@ const images = [
   "/BannerImg7.jpg",
 ]
 
-const Banner: React.FC = () => {
+const mobileImages = [
+   "/BannerImg5.jpg",
+ "/imge33Edited.jpg",
+  "/BannerImg1.jpg",
+  "/BannerImg2.jpg",
+  "/BannerImg3.jpg",
+  "/BannerImg4.jpg",
+  "/BannerImg7.jpg",
+]
+
   return (
-    <Carousel arrows autoplay autoplaySpeed={5000}>
-      {images.map((src, index) => (
-        <div key={index} className="w-full h-[60vh] relative">
-          <Image
-            src={src}
-            alt={`Banner ${index + 1}`}
-            fill
-            style={{ objectFit: "cover" }}
-            priority
-          />
-        </div>
-      ))}
-    </Carousel>
+    <>
+      {/* Desktop / Tablet */}
+      <div className="hidden md:block">
+        <Carousel arrows autoplay autoplaySpeed={5000}>
+          {desktopImages.map((src, index) => (
+            <div key={index} className="w-full h-[60vh] relative">
+              <Image
+                src={src}
+                alt={`Banner ${index + 1}`}
+                fill
+                priority
+                className="object-cover"
+              />
+            </div>
+          ))}
+        </Carousel>
+      </div>
+
+      {/* Mobile */}
+      <div className="block md:hidden">
+        <Carousel autoplay autoplaySpeed={5000}>
+          {mobileImages.map((src, index) => (
+            <div key={index} className="w-full h-[40vh] relative">
+              <Image
+                src={src}
+                alt={`Mobile Banner ${index + 1}`}
+                fill
+                priority
+                className="object-cover"
+              />
+            </div>
+          ))}
+        </Carousel>
+      </div>
+    </>
   )
 }
 
